5d81861ecac96128bf3c5e528f8438d5086264e9
Akseli Hulkkonen <akseli.hulkkonen@gmail.com>
2023-01-27 13:52:11 +0200

    BUG: fix task2

--- routes/users.js
+++ routes/users.js
@@ -6,6 +6,7 @@ const mongoose = require("mongoose")
 const {body, validationResult} = require("express-validator")
 const jwt = require("jsonwebtoken")
 const User = require("../models/Users")
+const Todo = require("../models/Todos")
 const passport  = require("passport-jwt")
 const validateToken = require("../auth/validation.js")
 
@@ -50,11 +51,11 @@ router.post('/user/login',
             tokenPayload,
             process.env.SECRET,
             {
-              expiresIn: 120
+              expiresIn: 120  
             },
             (err, token)=>{
               if(err) throw err;
-               return res.send({success: true,token: token})
+               return res.json({success: true,token})
             }
           )
         }
